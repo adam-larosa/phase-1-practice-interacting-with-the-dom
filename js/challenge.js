6c77947043e0b100fe248fb8597715f31bc63c41
adam-larosa <adam.larosa@flatironschool.com>
2021-09-28 16:04:06 -0700

    afterparty finish!  :)

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,37 @@
 
 
+
+
+
+let pause = false
+
+let countId = setInterval(increment, 1000)
+
+const pauseElement = document.getElementById('pause')
+
+const changeButtonText = () => {
+    if (!pause) {
+        pauseElement.textContent = "resume"
+        //change the button to one thing
+    } else {
+        pauseElement.textContent = "pause"
+        //change the button to something else
+    }
+}
+
+
+pauseElement.addEventListener('click', () => {
+
+    if (pause) {
+        countId = setInterval(increment, 1000)
+    } else {
+        clearInterval(countId)
+    }
+    changeButtonText()
+    pause = !pause
+    //do it here?
+})
+
 // grab the form element
 
 const formElement = document.querySelector('form')
@@ -58,7 +90,13 @@ const decrement = () => {
 }
 
 // add event to button
-minusElement.addEventListener('click', decrement)
+minusElement.addEventListener('click', () => {
+
+    if (!pause) {
+        decrement()
+    } 
+
+})
 
 function increment() {
     //we already grabbed the number element above (numberElement)
@@ -77,24 +115,43 @@ plusElement.addEventListener('click', increment)
 document.getElementById('heart').addEventListener('click', () => {
     //grab the container for new list elements
     const likeContainer = document.querySelector('.likes')
-    const currentNumber = numberElement.textContent
+    const currentNumber = numberElement.innerText
 
     const foundElement = document.getElementById(`likes-${currentNumber}`)
 
     if (foundElement) {
+        // we've already liked this number, the li is alread on the DOM, & we
+        // just need to change what's in that li
+
+        // grab that first sentence
         const fullText = foundElement.innerText
+
+        // split the sentence into an array, so each element in the array is
+        // a single word
         const words = fullText.split(' ')
-        const ourNewNumber = parseInt(words[4]) + 1
+
+        // grab the one part of the array with the current totalLikes
+        const totalLikes = words[4]
+
+        // change the value of the totalLikes to add one
+        const ourNewNumber = parseInt(totalLikes) + 1
+
+        //put the new number back in the array
         words[4] = ourNewNumber
-        foundElement.innerText = words.join(' ')
+
+        //join the array into a single sentence (i.e. "string")
+        const newLikesSentence = words.join(' ')
+
+        //append new sentence to DOM
+        foundElement.innerText = newLikesSentence
+
     } else {
-         //make a new li
+         //make a new li   <- because this is the first time we've like this
+                           // number
         const newLi = document.createElement('li')
         newLi.id = `likes-${currentNumber}`
         //put string in li
-        
         newLi.textContent = `${currentNumber} has been liked 1 times`
-        
         //append li to dom
         likeContainer.append(newLi)
     }
